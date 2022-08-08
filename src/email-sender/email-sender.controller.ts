015d2d4bc3f64a28d3088312b1f1e0c57217febb
Artyom Vorokhta <avorohta@newshift.ru>
2022-08-08 17:48:30 +0300

    lint fixes

--- src/email-sender/email-sender.controller.ts
+++ src/email-sender/email-sender.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { SendEmailDto } from './dto/send-email.dto';
 import { EmailSenderService } from './email-sender.service';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 config();
